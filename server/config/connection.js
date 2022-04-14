7be5f42b2a095ce237bb65a1709eb6620bc150d9
Synui <jonathon.schoenholz@yahoo.con>
2022-04-14 18:02:14 -0400

    Build

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/asl-game',
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/asl-game',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
