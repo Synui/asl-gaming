cc2a86012e6fe91ad843738644417321f78d800a
Synui <jonathon.schoenholz@yahoo.con>
2022-04-14 05:21:43 -0400

    Login and Signup functioning throught app

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Homepage = () => {
+    return(
+        
+        <div className="homepage">
+            {/* <Link to="/"> */}
+            <h1>ASL Gaming</h1>
+            {/* </Link> */}
+            <Link to="/memory-game">
+            <button className="start-button">Start</button>
+            </Link>
+            {/* <Link to="/practice"> */}
+            <button className="practice-button">Practice</button>
+            {/* </Link> */}
+        </div>
+    );
+}
+
+export default Homepage;
